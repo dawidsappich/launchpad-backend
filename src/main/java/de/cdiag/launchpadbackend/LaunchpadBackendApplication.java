b3e0b1fed9abdbf03e1275609b31235ead34634a
dawid sappich <dawid.sappich@cdi-ag.de>
2020-01-17 13:08:21 +0100

    initial commit

---
+++ src/main/java/de/cdiag/launchpadbackend/LaunchpadBackendApplication.java
@@ -0,0 +1,13 @@
+package de.cdiag.launchpadbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaunchpadBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LaunchpadBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/cdiag/launchpadbackend/LaunchpadBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package de.cdiag.launchpadbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaunchpadBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
