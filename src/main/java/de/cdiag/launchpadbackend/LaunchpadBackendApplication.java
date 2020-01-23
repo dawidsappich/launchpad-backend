7f8edda92923506483923339cead8e77359f8c31
dawid sappich <dawid.sappich@cdi-ag.de>
2020-01-23 09:45:09 +0100

    setup aws for profiling

--- src/main/java/de/cdiag/launchpadbackend/LaunchpadBackendApplication.java
+++ src/main/java/de/cdiag/launchpadbackend/LaunchpadBackendApplication.java
@@ -1,26 +1,50 @@
 package de.cdiag.launchpadbackend;
 
+import de.cdiag.launchpadbackend.aws.AwsConfig;
 import de.cdiag.launchpadbackend.model.*;
 import de.cdiag.launchpadbackend.service.UserService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import software.amazon.codeguruprofilerjavaagent.Profiler;
 
+import javax.annotation.PreDestroy;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 @SpringBootApplication
+@Slf4j
 public class LaunchpadBackendApplication {
 
+    private final AwsConfig awsConfig;
+    private final Profiler prof;
     @Autowired
     private UserService userService;
 
+
+    @Autowired
+    public LaunchpadBackendApplication(final AwsConfig _awsConfig) {
+        this.awsConfig = _awsConfig;
+        System.setProperty("aws.region", this.awsConfig.getRegion());
+        prof = this.awsConfig.awsCodeGuruProfiler();
+        prof.start();
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(LaunchpadBackendApplication.class, args);
+    }
+
+    @PreDestroy
+    private void stop() {
+        log.info("Stopping Profiler");
+        if (prof.isRunning()) {
+            prof.stop();
+        }
     }
 
     // seed data to database
@@ -67,7 +91,6 @@ public class LaunchpadBackendApplication {
             users.add(normalUser);
 
             userService.save(users);
-
 
 
         };
---
+++ src/main/java/de/cdiag/launchpadbackend/aws/AwsConfig.java
@@ -0,0 +1,43 @@
+package de.cdiag.launchpadbackend.aws;
+
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.codeguruprofilerjavaagent.Profiler;
+
+
+@Configuration
+@Data
+public class AwsConfig {
+
+    @Value("${aws.username}")
+    protected String awsUsername;
+
+    @Value("${aws.region}")
+    protected String region;
+
+    @Value("${aws.accessKey}")
+    protected String awsAccessKey;
+
+    @Value("${aws.secretKey}")
+    protected String awsSecretKey;
+
+    @Value("${aws.profiling.group.name}")
+    protected String profilingGroupName;
+
+    public Profiler awsCodeGuruProfiler() {
+        return new Profiler.Builder()
+                .profilingGroupName(profilingGroupName)
+                .awsRegionToReportTo(Region.of(this.region))
+                .awsCredentialsProvider(this.amazonAWSCredentials())
+                .build();
+    }
+
+    public AwsCredentialsProvider amazonAWSCredentials() {
+        return StaticCredentialsProvider.create(AwsBasicCredentials.create(getAwsAccessKey(),getAwsSecretKey()));
+    }
+}
