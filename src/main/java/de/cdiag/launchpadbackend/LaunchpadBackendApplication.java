7b1bbe6e0fec63b201608a3ffe34300472091e99
dawid sappich <dawid.sappich@cdi-ag.de>
2020-02-13 07:45:05 +0100

    remove aws configs

--- src/main/java/de/cdiag/launchpadbackend/LaunchpadBackendApplication.java
+++ src/main/java/de/cdiag/launchpadbackend/LaunchpadBackendApplication.java
@@ -1,21 +1,8 @@
 package de.cdiag.launchpadbackend;
 
-import de.cdiag.launchpadbackend.configuration.AwsConfig;
-import de.cdiag.launchpadbackend.model.*;
-import de.cdiag.launchpadbackend.service.UserService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import software.amazon.codeguruprofilerjavaagent.Profiler;
-
-import javax.annotation.PreDestroy;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 
 @SpringBootApplication
 @Slf4j
